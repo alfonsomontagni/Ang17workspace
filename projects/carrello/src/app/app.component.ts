85ab227509ad18fbc2b1e525b693e70df3c2aea9
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2024-01-17 08:41:54 +0100

    struttura base home settings cart

--- projects/carrello/src/app/app.component.ts
+++ projects/carrello/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
