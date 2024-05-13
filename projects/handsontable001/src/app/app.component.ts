7045cfe83091d8bfec94984d5b9740a1e9465251
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2024-05-13 11:33:42 +0200

    override handson table

--- projects/handsontable001/src/app/app.component.ts
+++ projects/handsontable001/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HandsonProva001Component } from './tables/handson-prova001/handson-prova001.component';
+import { CerticateGridComponent } from './ibm-certificate/certicate-grid/certicate-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HandsonProva001Component],
+  imports: [RouterOutlet,HandsonProva001Component,CerticateGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
